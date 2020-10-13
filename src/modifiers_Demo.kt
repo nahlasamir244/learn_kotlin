570bd0e317010fe89547763062f9342995cc0537
nahlasamir244 <nahla.s1995@gmail.com>
2020-10-13 13:42:29 -0700

    interfaces and abstract classes

--- src/modifiers_Demo.kt
+++ src/modifiers_Demo.kt
@@ -2,20 +2,56 @@
 //private : visible in same file or class (can be accessed)
 //internal : visible in same module
 //protected is not applicable for high level fun or classes
-open class Animal {
+//------------------------------------------------------------------------------
+
+//abstract class used to provide family behavior with no implementation
+
+abstract class Animal { // abstract class by default open
     private val a = 1
-    protected val b = 2
+    protected open val b = 2
+    protected abstract val abs: Int
     internal val c = 3
     var g = 6 //by default public
-    fun display(){ //by default public final
-
-    }
+    abstract fun display() //by default open and must be overridden
+    //other fun by default public final
+    //cannot create object of abstract class
 }
 
-class Dog : Animal() {
-    fun test(){
+class Dog(override val abs: Int, override var name: String) : Animal(), Pet, Mumble { //this is how to extend class and implement interface
+    //override abstract property = initialize it's value
+    override val b: Int = 1
+    fun test() {
         print("values are $b $c $g vs a is not visible")
     }
 
+    override fun display() {
+        TODO("Not yet implemented")
+    }
 
+    override fun eat() {
+        TODO("Not yet implemented")
+    }
+
+    override fun print() { // if class implement 2 interface has 2 normal methods with same name you must override the method
+        TODO("Not yet implemented")
+        super<Pet>.print()
+    }
+
+    //Data classes has copy() fun
+
+}
+
+interface Pet {
+    //all fun and properties in interface by default open
+    var name: String//all properties are abstract
+    abstract fun eat()
+    fun print() { //normal method : fun in interface can be not abstract and its open by default
+        println("pet name $name")
+    }
+}
+
+interface Mumble {
+    fun print() {
+        println("mumble")
+    }
 }
