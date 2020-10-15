e1629420bc00a370b4e30243d143952b1df5d7cf
nahlasamir244 <nahla.s1995@gmail.com>
2020-10-14 18:12:56 -0700

    high order functions 1

--- src/HighOrderFunctions.kt
+++ src/HighOrderFunctions.kt
@@ -1,5 +1,19 @@
 fun main(args: Array<String>) {
+    val test = Test()
+    println(test.addTwo(3, 4))
+
 
 }
 
-//high level functions or high order functions :  accept functions as paramters 
+//high level functions or high order functions :  accept functions as parameter or return function or do both
+//lamda : function with no name
+
+class Test {
+    fun addTwo(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}
+
+interface TestInterface {
+    fun execute(sun: Int)
+}
